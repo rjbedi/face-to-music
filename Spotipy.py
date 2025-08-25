53682b6336d0e2fb03477c24206b4a28b3673b44
Rajnish Bedi <rjbedi440@gmail.com>
2025-08-25 13:43:01 +0530

    Add files via upload

---
+++ Spotipy.py
@@ -0,0 +1,36 @@
+import spotipy
+import spotipy.oauth2 as oauth2
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+import time
+
+auth_manager = SpotifyClientCredentials('','')
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+def getTrackIDs(user, playlist_id):
+    track_ids = []
+    playlist = sp.user_playlist(user, playlist_id)
+    for item in playlist['tracks']['items']:
+        track = item['track']
+        track_ids.append(track['id'])
+    return track_ids
+
+def getTrackFeatures(id):
+    track_info = sp.track(id)
+
+    name = track_info['name']
+    album = track_info['album']['name']
+    artist = track_info['album']['artists'][0]['name']
+
+    track_data = [name, album, artist] 
+    return track_data
+
+emotion_dict = {0:"Angry",1:"Disgusted",2:"Fearful",3:"Happy",4:"Neutral",5:"Sad",6:"Surprised"}
+music_dist={0:"0l9dAmBrUJLylii66JOsHB?si=e1d97b8404e34343",1:"1n6cpWo9ant4WguEo91KZh?si=617ea1c66ab6446b ",2:"4cllEPvFdoX6NIVWPKai9I?si=dfa422af2e8448ef",3:"0deORnapZgrxFY4nsKr9JA?si=7a5aba992ea14c93",4:"4kvSlabrnfRCQWfN0MgtgA?si=b36add73b4a74b3a",5:"1n6cpWo9ant4WguEo91KZh?si=617ea1c66ab6446b",6:"37i9dQZEVXbMDoHDwVN2tF?si=c09391805b6c4651"}
+
+'''
+Code can def be modularised into a function but i tried to write it when i was extremely sleepy so thought screw it and repeated code block
+
+Uncomment for fetching updated playlists
+'''
